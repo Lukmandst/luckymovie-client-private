f2d1fe3f5b13123a4f67ca051d732f3a0f1546c9
Lukmandst <lukmandwi11@gmail.com>
2022-07-09 07:37:18 +0700

    add formatter and get user history func

--- src/helper/formatter.jsx
+++ src/helper/formatter.jsx
@@ -3,3 +3,7 @@ export const currencyFormatter = new Intl.NumberFormat("id-ID", {
     currency: "IDR",
     minimumFractionDigits: 0
 })
+export function formatPhoneNumber(number) {
+    const result = number.replace(/(\d{3})(\d{3})(\d{4})/,'$1-$2-$3')
+    return result
+  }
