15eb3ed8dece73a24c73f1d7c17fed57cc18e5bb
Lukmandst <lukmandwi11@gmail.com>
2022-07-10 04:46:59 +0700

    fix implement movie details

--- src/pages/order-page/index.jsx
+++ src/pages/order-page/index.jsx
@@ -2,6 +2,7 @@ import Footer from 'components/Footer'
 import Header from 'components/header/Header'
 import styles from './Order.module.css'
 import Seat from 'components/seat/Seat'
+import Loading from 'components/loading/Loading'
 
 
 const OrderPage = () => {
@@ -9,6 +10,7 @@ const OrderPage = () => {
     <>
     <Header/>
     <div className={styles.container}>
+      <Loading/>
     <Seat/>
     </div>
     <Footer/>
