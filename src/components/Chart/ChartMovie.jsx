ef885ec4d3ae78e808c6dd6dea938a2bbc40be98
Lukmandst <lukmandwi11@gmail.com>
2022-07-12 05:32:07 +0700

    fix chart

--- src/components/Chart/ChartMovie.jsx
+++ src/components/Chart/ChartMovie.jsx
@@ -1,56 +1,69 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-  import { Line } from 'react-chartjs-2';
-  import styles from './Chart.module.css'
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+const ChartMovie = ({ movies, filter }) => {
+  console.log(movies.detail);
+  const labels =
+    filter === "weekly"
+      ? movies && movies.detail.map((item) => item.day_name)
+      : movies.detail.map((item) => item.month_name);
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Income",
+        // data: Object.entries(group).map(item => item[1].map(day=> Number(day.total))),
+        data: movies && movies.detail.map((item) => item.total),
+        //  movies.map((revenue) => revenue.total),
+        borderColor: "#5F2EEA",
+        backgroundColor: "#9570FE",
+      },
+    ],
+  };
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: movies && movies.title,
+      },
+    },
+  };
+  return (
+    <>
+      
+        <div className={styles.space2}>
+          <div className={`${styles.singleChart}`}>
+            <Line options={options} data={data} />
+          </div>
+        
+      </div>
+    </>
   );
+};
 
-const ChartMovie = ({movies = []}) => {
-    console.log(movies.map(day=>day.day_name));
-    const labels = movies.map(day=>day.day_name)
-    const data = {
-        labels,
-        datasets: [
-          {
-            label: 'Dataset 1',
-            // data: Object.entries(group).map(item => item[1].map(day=> Number(day.total))),
-            data : movies.map(revenue=> revenue.total),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-        ],
-    };
-    const options = {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: movies[0].title,
-          },
-        },
-    };
-    return <Line data={data} options={options}/>
-}
-
-export default ChartMovie
+export default ChartMovie;
