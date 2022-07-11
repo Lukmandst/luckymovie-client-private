3dc06010cff01b2b7ca942518b52de9ab158d70d
Lukmandst <lukmandwi11@gmail.com>
2022-07-11 17:08:22 +0700

    fix navbar condition

---
+++ src/components/header/HeaderAfterLoginNormal.jsx
@@ -0,0 +1,58 @@
+import { GetUser } from "modules/axios";
+import Image from "next/image";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import dummy from "../../assets/img/defaultProfile.png";
+import styles from "./Header.module.css";
+
+
+function HeaderAfterLogin({ showToggle, setShowToggle, handleShowLogout }) {
+  const { token } = useSelector((state) => state.auth);
+  const { user, isLoading, isError } = GetUser(token);
+  return (
+    <div className="d-flex align-items-center">
+      <li className="mx-2 nav-item">
+        <span style={{ fontWeight: "600" }}>
+          Welcome {user && user.first_name} !
+        </span>
+      </li>
+      <li
+        className="mx-2 nav-item"
+        style={{
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src={
+            user && !user.picture
+              ? dummy
+              : user
+              ? `${user && user.picture}`
+              : "/"
+          }
+          alt="profile-image"
+          objectFit="cover"
+          layout="fixed"
+          width={65}
+          height={65}
+          style={{ borderRadius: "50px", cursor: "pointer" }}
+          onClick={() => setShowToggle(!showToggle)}
+        />
+        {showToggle && (
+          <div className={styles.dropProfile}>
+            <Link href={"/profile"}>
+              <span className={styles.dropVal}>Profile</span>
+            </Link>
+            <span className={styles.dropVal} onClick={handleShowLogout}>
+              Log out
+            </span>
+          </div>
+        )}
+      </li>
+    </div>
+  );
+}
+
+export default HeaderAfterLogin;
---
+++ src/components/header/HeaderAfterLoginResponsive.jsx
@@ -0,0 +1,69 @@
+import { GetUser } from "modules/axios";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutAction } from "redux/actionCreators/auth";
+import dummy from "../../assets/img/defaultProfile.png";
+import styles from "./Header.module.css";
+
+function HeaderAfterLoginResponsive({ showToggle, handleShowLogout }) {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+  const { user, isLoading, isError } = GetUser(token);
+  const router = useRouter();
+  useEffect(() => {
+    if (isError) {
+      dispatch(logoutAction());
+    }
+  }, [dispatch, isError]);
+  return (
+    <>
+      <div className="p-2 py-3">
+        <Image
+          src={
+            user && !user.picture
+              ? dummy
+              : user
+              ? `${user && user.picture}`
+              : "/"
+          }
+          alt="profile-image"
+          objectFit="cover"
+          layout="fixed"
+          width={65}
+          height={65}
+          style={{ borderRadius: "50px", cursor: "pointer" }}
+        />
+        {showToggle && (
+          <div className={styles.dropProfile}>
+            <Link href={"/profile"}>
+              <span className={styles.dropVal}>Profile</span>
+            </Link>
+            <span className={styles.dropVal}>Log out</span>
+          </div>
+        )}
+      </div>
+      <div className="p-2 py-3 d-flex">
+        <div>
+          <button
+            className="btn btn-outline"
+            onClick={() => {
+              router.push("/profile");
+            }}
+          >
+            Profile
+          </button>
+        </div>
+        <div>
+          <button className="btn" onClick={handleShowLogout}>
+            Log Out
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeaderAfterLoginResponsive;
