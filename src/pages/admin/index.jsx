4494e3b8f0dabf6f9e15e0c2a3e5365c455fb907
andrei <andre4acc@gmail.com>
2022-07-12 20:52:05 +0700

    fix admin page

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -9,6 +9,7 @@ import hiflix from "../../assets/img/hiflix.png";
 import cineone from "../../assets/img/cineone.png";
 import biPlus from "../../assets/img/bi_plus.png";
 import ChartSales from "components/Chart/Chart";
+import axios from "axios";
 import {
   GetSalesLocation,
   GetSalesMovie,
@@ -17,12 +18,16 @@ import {
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import ChartMovie from "components/Chart/ChartMovie";
+import { useRouter } from "next/router";
+
+
 
 const Admin = () => {
   const [filter, setFilter] = useState("weekly");
   const [movieSales, setMovieSales] = useState([]);
   const [movie, setMovie] = useState(true);
   const [location, setLocation] = useState(false);
+  const [detailMovie, setDetailMovie] = useState([])
 
   // const[location_id, setLocationId] = useState(27)
   const { token } = useSelector((state) => state.auth);
@@ -68,7 +73,20 @@ const Admin = () => {
   // }, [filter, token])
 
   // console.log(allMovieSales.map(result=> result.title));
-  const getAllRevenueMovie = () => {};
+  // const {id } = useRouter()
+  useEffect(()=>{
+    const getDetailMovie = async ()=>{
+      try {
+        const result = await axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/movies/15`)
+        console.log(result);
+        setDetailMovie(result.data.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getDetailMovie()
+  }, [])
+
 
   return (
     <>
@@ -77,94 +95,6 @@ const Admin = () => {
       </Head>
       <Header />
       <div className={styles.container}>
-        <div className={styles.top}>
-          <div className={styles.movieDescription}>
-            <span>Movie Description</span>
-            <div className={styles.cardDesc}>
-              <div className={styles.topCard}>
-                <div className={styles.cardImage}>
-                  <Image src={spiderman} alt="movie_img" />
-                </div>
-                <div className={styles.cardInput}>
-                  <label htmlFor="">Movie Name</label>
-                  <input type="text" placeholder="Spiderman" />
-                  <label htmlFor="">Category</label>
-                  <input type="text" placeholder="Action, Adventure, Sci-Fi" />
-                  <label htmlFor="">Release Date</label>
-                  <input type="text" placeholder="release-date" />
-                  <div className={styles.duration}>
-                    <label htmlFor="">Duration (Hour/Minute)</label>
-                    <div className={styles.inputHour}>
-                      <input type="text" placeholder="2" />
-                      <input type="text" placeholder="12" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.bottomCard}>
-                <div className={styles.director}>
-                  <label htmlFor="">Director</label>
-                  <input type="text" />
-                </div>
-                <div className={styles.director}>
-                  <label htmlFor="">Cast</label>
-                  <input type="text" />
-                </div>
-              </div>
-              <div className={styles.bottomCard}>
-                <div className={styles.synopsis}>
-                  <span>Synopsis</span>
-                  <textarea name="" id="" cols="30" rows="10"></textarea>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.premiere}>
-            <span>Premiere Location</span>
-            <div className={styles.premiereCard}>
-              <select name="" id="">
-                <option value="">Semarang</option>
-                <option value="">Purwokerto</option>
-                <option value="">Yogyakarta</option>
-              </select>
-              <div className={styles.premiereImg}>
-                <Image src={ibv} alt="IBV" />
-                <Image src={hiflix} alt="hiflix" />
-                <Image src={cineone} alt="cineone" />
-              </div>
-              <div className={styles.premiereImg}>
-                <Image src={ibv} alt="IBV" />
-                <Image src={hiflix} alt="hiflix" />
-                <Image src={cineone} alt="cineone" />
-              </div>
-              <div className={styles.premiereImg}>
-                <Image src={ibv} alt="IBV" />
-                <Image src={hiflix} alt="hiflix" />
-                <Image src={cineone} alt="cineone" />
-              </div>
-            </div>
-            <div className={styles.showtime}>
-              <span>Showtimes</span>
-              <div className={styles.showtimeCard}>
-                <div className="inputDate">
-                  <input type="date" />
-                </div>
-                <div className={styles.setTime}>
-                  <Image src={biPlus} alt="plus" />
-                  <span>08:30</span>
-                  <span>08:30</span>
-                  <span>08:30</span>
-                </div>
-                <div className={styles.setTime}>
-                  <span>09:30</span>
-                  <span>08:30</span>
-                  <span>08:30</span>
-                  <span>08:30</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         {!movie ? (
           <ChartSales
             jakarta={Jakarta}
@@ -203,10 +133,8 @@ const Admin = () => {
                 
               </div>
             </div>
-
             <div
-              className={styles.pembungkus}
-              
+              className={`${styles.pembungkus}`}
             >
               {allMovieSales ? (
                 Array.isArray(allMovieSales) &&
